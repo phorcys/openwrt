c86ad3c16de3d3e78d235b644e6b05635db4f05d
phorcys <phorcys@126.com>
2021-08-10 08:04:49 +0800

    add loongarch grub2 and openwrt image generation

--- target/linux/loongson/image/3a5000.mk
+++ target/linux/loongson/image/3a5000.mk
@@ -1,23 +1,7 @@
-#
-# Loongson Profiles
-#
-
-
-
-define Device/3a5000
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  KERNEL_SIZE := 16384k
-  IMAGE_SIZE := 28672k
-  UBINIZE_OPTS := -E 5
-  IMAGES += kernel.bin rootfs.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  IMAGE/kernel.bin := append-kernel
-  IMAGE/rootfs.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  DEVICE_TITLE := Loongson 3A5000
-  DEVICE_PACKAGES := \
-	 wpad-basic
+define Device/generic
+  DEVICE_TITLE := Loongson 3A5000 Deice
+  DEVICE_PACKAGES += kmod-e1000e kmod-e1000 \
+	 kmod-r8169 kmod-fs-vfat
+  GRUB2_VARIANT := generic
 endef
-TARGET_DEVICES += 3a5000
-
-
+TARGET_DEVICES += generic
